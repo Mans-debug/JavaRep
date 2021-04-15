6adbabd4f1b88bafe597988b61787e11f83528ef
“Mansur <mansurminnikaev@gmail.com>
2021-04-15 19:31:41 +0300

    1.0 version, ready to be tested

--- 15.04.21/src/Bot.java
+++ 15.04.21/src/Bot.java
@@ -1,3 +1,4 @@
+import javax.jws.Oneway;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -5,18 +6,24 @@ import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.stream.Stream;
 //todo how to return int and void or int and string
-public class Bot<T>
+public class Bot
 {
-    HashMap<String, Method> commands;
+    private HashMap<String, Method> commands;
 
     public Bot() {
         commands = new HashMap<>();
 
-        for (Method m : this.getClass().getDeclaredMethods()) {
+        Class botCommandsClass = new BotCommands().getClass();
+        for (Method m : botCommandsClass.getDeclaredMethods()) {
             if (!m.isAnnotationPresent(Command.class))
                 continue;
 
             Command cmd = m.getAnnotation(Command.class);
+
+            //if a method is still in progress, we don't include it in the map
+            if(cmd.inProgress())
+                continue;
+
             for (String name : cmd.possibleNames())
                 commands.put(name, m);
         }
@@ -52,7 +59,7 @@ public class Bot<T>
 //    }
 
 
-    public <T> T commandProcessor(String input) throws InputMismatchException
+    public String commandProcessor(String input) throws InputMismatchException
     {
         if(input.isEmpty())
             throw new InputMismatchException("Wrong input");
@@ -61,85 +68,23 @@ public class Bot<T>
         String command = strings[0].toLowerCase();
         String[] args = Arrays.copyOfRange(strings,1,  strings.length);
 
-        Method m =commands.get(command);
+        Method m = commands.get(command);
 
         if(m == null)
             throw new InputMismatchException("There is no such method");
+
         try
         {
-            if(m.getReturnType().equals(Integer.TYPE))
-           return ReturnEnum.IntegerType.comeback( Integer.toString( (Integer)
-                    m.invoke(this, Stream.of(args)
-                    .map(x -> Integer.parseInt(x))
-                    .toArray())));
-            else if(String.class.equals(m.getReturnType()))
-                return ReturnEnum.StringType.comeback((String)
-                        m.invoke(this, (Object) args));
-
-
-        } catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-        } catch (InvocationTargetException e)
-        {
-            e.printStackTrace();
+           return (String) m.invoke(new BotCommands(), (Object) args);
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            System.out.println("Something went wrong while calling the method: " + m.getName());
         }
-        return null;
+            return "Something went wrong while processing the input command";
     }
 
-    @Command(possibleNames = {"power"},
-            args = " int the number, int the power",
-    description = "returns a number X to the power Y",
-    inProgress = false)
-    int power(int x, int power)
-    {
-        int res = 1;
-        if(power == 0)
-            return 1;
-        for (int i = 0; i < power; i++)
-        {
-            res *= x;
-        }
-        return res;
-    }
 
-    @Command(possibleNames = {"factorial of", "factorial", "fact"},
-    args = "the factorial of X",
-    description = "returns factorial of X",
-    inProgress = false)
-    int factorial(int x)
-    {
-        int res = 1;
-
-        if (x == 0)
-            return 0;
-        for (int i = 1; i <= x; i++)
-        {
-            res *= i;
-        }
-        System.out.println(res);
-        return res;
-    }
-
-    @Command(possibleNames = {"help"},
-    description = "prints a list of callable commands", inProgress = false)
-    public String help(String[] args) {
-        StringBuilder builder = new StringBuilder("Я умею в такие команды:\n");
-
-        for (Method m : this.getClass().getDeclaredMethods()) {
-            if (!m.isAnnotationPresent(Command.class))
-                continue;
-
-            Command cmd = m.getAnnotation(Command.class);
-            builder.append(Arrays.toString(cmd.possibleNames())).append(": ").append(cmd.description()).append(" - ").append(cmd.args()).append("\n");
-        }
-
-        return builder.toString();
-    }
 
 
 
--- 15.04.21/src/Main.java
+++ 15.04.21/src/Main.java
@@ -3,8 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         Bot bot = new Bot();
-
-
-        System.out.println(bot.commandProcessor("fact 4"));
+        String string = bot.commandProcessor("reverse hello world");
+        System.out.println(string);
     }
 }
