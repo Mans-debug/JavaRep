06b2d3ed89d2a91ec287a52a3400c9e8c006adcb
“Mansur <mansurminnikaev@gmail.com>
2021-05-12 11:51:11 +0300

    added another way in third task@ @

--- 13.05.21/src/ThirdTask.java
+++ 13.05.21/src/ThirdTask.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -12,13 +9,25 @@ public class ThirdTask {
         File file = new File("C:\\Users\\mansu\\IdeaProjects\\13.05.21\\task3.txt");
         Scanner sc = new Scanner(file);
         HashMap<String, Integer> map = new HashMap<>();
-
-        sc.tokens()
+        //one way
+       sc.tokens()
                 .map(x -> x.split("[|]"))
                 .map(x -> map.containsKey(x[0]) ?
                         map.put(x[0], map.get(x[0]) + Integer.parseInt(x[1])) :
                         map.put(x[0], Integer.parseInt(x[1])));
 
         map.entrySet().stream().forEach(x -> System.out.println(x.getKey() + " " + x.getValue()));
+        
+        //another way
+        String[] s = sc.tokens().toArray(String[]::new);
+        var map1 = Arrays.stream(s).map(x -> x.split("[|]"))
+                .collect(Collectors.groupingBy(x -> x[0]))
+                .entrySet()
+                .stream()
+                .collect
+                        (Collectors.toMap(x -> x.getKey(), x -> x.getValue()
+                                .stream().map(a -> Integer.parseInt(a[1])).reduce((z, t) -> z + t ).get()));
+
+        map1.entrySet().stream().forEach(x -> System.out.println(x.getKey() + " " + x.getValue()));
     }
 }
---
+++ 13.05.21/src/Задачки.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Задачки {
+    public static void main(String[] args) {
+
+    }
+}
